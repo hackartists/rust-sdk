7e265358fa6acb0f431fe8033cafdc3e2e1dca17
hackartists <hackartists@gmail.com>
2025-02-02 08:54:37 +0900

    feat(cdk): add support for custom stack names and cron

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -5,8 +5,13 @@ import { CdkStack } from "../lib/cdk-stack";
 
 const app = new cdk.App();
 
+let stackName = `${process.env.PROJECT}-${process.env.SERVICE}-${process.env.ENV}-stack`;
+if (process.env.STACK) {
+  stackName = process.env.STACK;
+}
+
 new CdkStack(app, `AppStack`, {
-  stackName: `${process.env.PROJECT}-${process.env.SERVICE}-${process.env.ENV}-stack`,
+  stackName,
 
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
