c6e1cde7251654346d7ea6d7cf7c32784c58b43d
hackartists <hackartists@gmail.com>
2025-02-04 21:12:34 +0900

    refactor(tests): organize action_by_id_empty_param.rs into a module

--- packages/by-macros/tests/action_by_id_empty_param.rs
+++ packages/by-macros/tests/action_by_id_empty_param.rs
@@ -1,99 +1,103 @@
-#![allow(unused)]
-use std::time::SystemTime;
-
 #[cfg(feature = "server")]
-use by_axum::aide;
-use by_macros::api_model;
+mod empty_param_tests {
+    #![allow(unused)]
+    use std::time::SystemTime;
 
-#[derive(Debug, Default, Clone, serde::Serialize, serde::Deserialize)]
-#[cfg_attr(feature = "server", derive(schemars::JsonSchema, aide::OperationIo))]
-pub struct QueryResponse<T> {
-    pub items: Vec<T>,
-    pub total_count: i64,
-}
+    #[cfg(feature = "server")]
+    use by_axum::aide;
+    use by_macros::api_model;
 
-impl<T> From<(Vec<T>, i64)> for QueryResponse<T> {
-    fn from((items, total_count): (Vec<T>, i64)) -> Self {
-        QueryResponse { items, total_count }
+    #[derive(Debug, Default, Clone, serde::Serialize, serde::Deserialize)]
+    #[cfg_attr(feature = "server", derive(schemars::JsonSchema, aide::OperationIo))]
+    pub struct QueryResponse<T> {
+        pub items: Vec<T>,
+        pub total_count: i64,
     }
-}
 
-pub type Result<T> = std::result::Result<T, by_types::ApiError<String>>;
+    impl<T> From<(Vec<T>, i64)> for QueryResponse<T> {
+        fn from((items, total_count): (Vec<T>, i64)) -> Self {
+            QueryResponse { items, total_count }
+        }
+    }
 
-#[api_model(base = "/models", table = action_empty_params, iter_type=QueryResponse, action_by_id = delete, action = [no_param, empty])]
-pub struct ActionEmptyParamModel {
-    #[api_model(summary, primary_key, read_action = find_by_id)]
-    pub id: String,
-    #[api_model(summary, auto = [insert])]
-    pub created_at: i64,
-    #[api_model(summary, auto = [insert, update])]
-    pub updated_at: i64,
+    pub type Result<T> = std::result::Result<T, by_types::ApiError<String>>;
 
-    #[api_model(summary)]
-    pub name: String,
-}
+    #[api_model(base = "/models", table = action_empty_params, iter_type=QueryResponse, action_by_id = delete, action = [no_param, empty])]
+    pub struct ActionEmptyParamModel {
+        #[api_model(summary, primary_key, read_action = find_by_id)]
+        pub id: String,
+        #[api_model(summary, auto = [insert])]
+        pub created_at: i64,
+        #[api_model(summary, auto = [insert, update])]
+        pub updated_at: i64,
 
-#[tokio::test]
-async fn test_action_by_id_empty_param() {
-    let cli = ActionEmptyParamModel::get_client("test");
-    cli.delete("0");
+        #[api_model(summary)]
+        pub name: String,
+    }
 
-    cli.no_param();
-    cli.empty();
+    #[tokio::test]
+    async fn test_action_by_id_empty_param() {
+        let cli = ActionEmptyParamModel::get_client("test");
+        cli.delete("0");
 
-    let pool: sqlx::Pool<sqlx::Postgres> = sqlx::postgres::PgPoolOptions::new()
-        .max_connections(5)
-        .connect(
-            option_env!("DATABASE_URL")
-                .unwrap_or("postgres://postgres:postgres@localhost:5432/test"),
-        )
-        .await
-        .unwrap();
-    let now = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
+        cli.no_param();
+        cli.empty();
 
-    let name = format!("test-{}", now);
+        let pool: sqlx::Pool<sqlx::Postgres> = sqlx::postgres::PgPoolOptions::new()
+            .max_connections(5)
+            .connect(
+                option_env!("DATABASE_URL")
+                    .unwrap_or("postgres://postgres:postgres@localhost:5432/test"),
+            )
+            .await
+            .unwrap();
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
 
-    let repo = ActionEmptyParamModel::get_repository(pool);
-    repo.create_table().await;
-    let res = repo.insert(name.clone()).await;
-    assert_eq!(res.is_ok(), true, "{:?}", res);
+        let name = format!("test-{}", now);
 
-    let res = res.unwrap();
-    assert_eq!(res.name, name.clone());
+        let repo = ActionEmptyParamModel::get_repository(pool);
+        repo.create_table().await;
+        let res = repo.insert(name.clone()).await;
+        assert_eq!(res.is_ok(), true, "{:?}", res);
 
-    let res = repo
-        .update(
-            &res.id,
-            ActionEmptyParamModelRepositoryUpdateRequest {
-                name: Some("test".to_string()),
-            },
-        )
-        .await;
-    assert_eq!(res.is_ok(), true);
-    let res = res.unwrap();
-    assert_eq!(res.name, "test".to_string());
+        let res = res.unwrap();
+        assert_eq!(res.name, name.clone());
 
-    let res = repo
-        .update(
-            &res.id,
-            ActionEmptyParamModelRepositoryUpdateRequest::new().with_name("test-2".to_string()),
-        )
-        .await;
-    assert_eq!(res.is_ok(), true);
-    let res = res.unwrap();
-    assert_eq!(res.name, "test-2".to_string());
+        let res = repo
+            .update(
+                &res.id,
+                ActionEmptyParamModelRepositoryUpdateRequest {
+                    name: Some("test".to_string()),
+                },
+            )
+            .await;
+        assert_eq!(res.is_ok(), true);
+        let res = res.unwrap();
+        assert_eq!(res.name, "test".to_string());
 
-    let id = res.id.clone();
-    let res = repo.delete(&res.id).await;
-    assert_eq!(res.is_ok(), true);
+        let res = repo
+            .update(
+                &res.id,
+                ActionEmptyParamModelRepositoryUpdateRequest::new().with_name("test-2".to_string()),
+            )
+            .await;
+        assert_eq!(res.is_ok(), true);
+        let res = res.unwrap();
+        assert_eq!(res.name, "test-2".to_string());
 
-    let res = repo
-        .find_one(&&ActionEmptyParamModelReadAction::new().find_by_id(id))
-        .await;
-    assert_eq!(res.is_err(), true);
+        let id = res.id.clone();
+        let res = repo.delete(&res.id).await;
+        assert_eq!(res.is_ok(), true);
 
-    let req_by_id = ActionEmptyParamModelByIdAction::Delete(ActionEmptyParamModelDeleteRequest {});
+        let res = repo
+            .find_one(&&ActionEmptyParamModelReadAction::new().find_by_id(id))
+            .await;
+        assert_eq!(res.is_err(), true);
+
+        let req_by_id =
+            ActionEmptyParamModelByIdAction::Delete(ActionEmptyParamModelDeleteRequest {});
+    }
 }
