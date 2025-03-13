9f03179e1d407fe8f2c213012206f1af06d0055c
hackartists <hackartists@gmail.com>
2025-03-14 07:59:51 +0900

    feat(workspace): add bdk package and update dependencies
    Added new ~bdk~ package to the workspace with necessary dependencies and features. Updated ~Cargo.toml~ and ~Cargo.lock~ to include ~bdk~ and other related dependencies.

---
+++ packages/bdk/src/lib.rs
@@ -0,0 +1,40 @@
+pub mod prelude {
+    pub use by_macros;
+    pub use by_types;
+    pub use dioxus_translate;
+    pub use reqwest;
+    pub use rest_api;
+    pub use tracing;
+    pub use validator;
+
+    #[cfg(feature = "fe")]
+    pub use btracing;
+    #[cfg(feature = "fe")]
+    pub use by_components;
+    #[cfg(feature = "fe")]
+    pub use dioxus;
+    #[cfg(feature = "fe")]
+    pub use dioxus::prelude::*;
+    #[cfg(feature = "fe")]
+    pub use dioxus_aws;
+    #[cfg(feature = "fe")]
+    pub use dioxus_logger;
+    #[cfg(feature = "fe")]
+    pub use dioxus_popup;
+
+    #[cfg(feature = "server")]
+    pub use dioxus::fullstack;
+    #[cfg(feature = "server")]
+    pub use dioxus::fullstack::prelude::*;
+
+    #[cfg(feature = "be")]
+    pub use aide;
+    #[cfg(feature = "be")]
+    pub use bigdecimal;
+    #[cfg(feature = "be")]
+    pub use by_axum;
+    #[cfg(feature = "be")]
+    pub use schemars;
+    #[cfg(feature = "be")]
+    pub use sqlx;
+}
