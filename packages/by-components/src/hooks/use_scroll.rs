c7e2d2702ecec0a220e058bbab0a84e6d98b2a08
hackartists <hackartists@gmail.com>
2025-03-18 17:47:53 +0900

    feat(hooks): add scroll direction and bottom detection to use_scroll
    Added ~ScrollDirection~ enum to indicate scroll direction (Up, Down). Updated ~use_scroll~ to include ~isBottom~ detection and pass scroll direction to the callback.

--- packages/by-components/src/hooks/use_scroll.rs
+++ packages/by-components/src/hooks/use_scroll.rs
@@ -5,23 +5,37 @@ pub struct UseScroll {
     pub callback: Callback<(), Task>,
 }
 
+pub enum ScrollDirection {
+    Up,
+    Down,
+}
+
 pub fn use_scroll<F>(mut callback: F)
 where
-    F: FnMut(f64, f64) + 'static + Copy,
+    F: FnMut(f64, f64, bool, ScrollDirection) + 'static + Copy,
 {
     let callback = use_callback(move |_| {
         let script = r#"
             window.addEventListener('scroll', () => {
-                dioxus.send(`${window.scrollY},${window.scrollX}`);
+                let scrollY = window.scrollY;
+                let scrollX = window.scrollX;
+                let scrollHeight = document.documentElement.scrollHeight-1;
+                let innerHeight = window.innerHeight;
+
+                let isBottom = (scrollY + innerHeight) >= scrollHeight;
+
+                dioxus.send(`${scrollY},${scrollX},${isBottom}`);
             });
         "#;
         let mut eval = document::eval(script);
 
         spawn(async move {
+            let mut y = 0.0;
+
             loop {
                 let pos = eval.recv::<String>().await.unwrap_or_default();
                 let mut pos = pos.split(',');
-                let y = pos
+                let new_y = pos
                     .next()
                     .unwrap_or_default()
                     .parse::<f64>()
@@ -32,7 +46,20 @@ where
                     .parse::<f64>()
                     .unwrap_or_default();
 
-                callback(x, y);
+                let is_bottom = pos
+                    .next()
+                    .unwrap_or_default()
+                    .parse::<bool>()
+                    .unwrap_or_default();
+                let direction = if new_y > y {
+                    ScrollDirection::Down
+                } else {
+                    ScrollDirection::Up
+                };
+
+                y = new_y;
+
+                callback(x, y, is_bottom, direction);
             }
         })
     });
