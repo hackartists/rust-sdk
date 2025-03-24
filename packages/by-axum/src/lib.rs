5c20252f58818c882a32a22a0c423858a08ec43b
hackartists <hackartists@gmail.com>
2025-03-24 12:33:24 +0900

    feat: update logging configuration to use RUST_LOG environment variable
    Updated the logging configuration to use the ~RUST_LOG~ environment variable with a default value of "info".

--- packages/by-axum/src/lib.rs
+++ packages/by-axum/src/lib.rs
@@ -17,10 +17,11 @@ pub mod rest_api_adapter;
 pub use by_types::ApiError;
 pub type Result<T, E> = std::result::Result<Json<T>, ApiError<E>>;
 pub use schemars;
+use tracing_subscriber::EnvFilter;
 
 pub fn new() -> BiyardRouter {
     let _ = tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_env_filter(EnvFilter::from(option_env!("RUST_LOG").unwrap_or("info")))
         .with_file(true)
         .with_line_number(true)
         .with_thread_ids(true)
