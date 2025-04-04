5cbeb0b6aed4916f16c6db1d5c86227b2a81692f
victor138128 <yepp0517@naver.com>
2025-04-04 11:26:46 +0900

    update rich text function

--- packages/by-components/src/rich_texts/rich_text.rs
+++ packages/by-components/src/rich_texts/rich_text.rs
@@ -3,58 +3,97 @@ use wasm_bindgen::{prelude::Closure, JsCast, JsValue};
 use web_sys::js_sys::eval;
 
 #[component]
-pub fn RichText(content: String, onchange: EventHandler<String>) -> Element {
-    use_effect(move || {
-        let document = web_sys::window().unwrap().document().unwrap();
+pub fn RichText(
+    #[props(default = "rich-text".to_string())] id: String,
+    content: String,
+    onchange: EventHandler<String>,
+) -> Element {
+    use_effect({
+        let id = id.clone();
+        move || {
+            let id = id.clone();
+            let js = format!(
+                r#"
+                (function tryInit() {{
+                    let editor = document.getElementById("{id}");
+                    if (editor && window.Quill && !editor.__quill) {{
+                        editor.__quill = new Quill(editor, {{ theme: "snow" }});
 
-        let script = r#"
-                setTimeout(() => {
-                    let editor = document.querySelector(".rich-text-editor");
-                    if (editor && !editor.__quill) {
-                        editor.__quill = new Quill(editor, { theme: "snow" });
-                        editor.__quill.root.innerHTML = editor.getAttribute("data-default-value") || "";
+                        let toolbar = editor.previousElementSibling;
+                        if (toolbar) {{
+                            toolbar.style.width = "100%";
+                            toolbar.style.maxWidth = "none";
+                            toolbar.style.boxSizing = "border-box";
+                        }}
 
-                        let toolbar = document.querySelector(".ql-toolbar");
-                        if (toolbar) {
-                            toolbar.style.width = "100%";   
-                            toolbar.style.maxWidth = "none"; 
-                        }
-                    
-                        editor.__quill.on('text-change', function(delta, oldDelta, source) {
-                            let new_content = editor.__quill.root.innerHTML;
-                            window.__quill_content = new_content;
-                            document.dispatchEvent(new Event('content-updated')); 
-                        });
-                    }
-                }, 500);
-            "#;
+                        editor.__quill.on('text-change', function(delta, oldDelta, source) {{
+                            window.__quill_content = editor.__quill.root.innerHTML;
+                            document.dispatchEvent(new Event('content-updated'));
+                        }});
+                    }} else {{
+                        setTimeout(tryInit, 200);
+                    }}
+                }})();
+                "#
+            );
 
-        spawn(async move {
-            let _ = eval(&script);
-        });
+            spawn(async move {
+                let _ = eval(&js);
+            });
 
-        let closure = Closure::wrap(Box::new(move |_event: JsValue| {
-            let editor = web_sys::window()
+            let closure = Closure::wrap(Box::new(move |_event: JsValue| {
+                let editor = web_sys::window()
+                    .unwrap()
+                    .document()
+                    .unwrap()
+                    .get_element_by_id(&id)
+                    .unwrap();
+
+                let html = editor
+                    .dyn_ref::<web_sys::HtmlElement>()
+                    .unwrap()
+                    .inner_html();
+
+                onchange.call(html);
+            }) as Box<dyn FnMut(JsValue)>);
+
+            web_sys::window()
                 .unwrap()
                 .document()
                 .unwrap()
-                .query_selector(".rich-text-editor")
-                .unwrap()
+                .add_event_listener_with_callback(
+                    "content-updated",
+                    closure.as_ref().unchecked_ref(),
+                )
                 .unwrap();
 
-            let html = editor
-                .dyn_ref::<web_sys::HtmlElement>()
-                .unwrap()
-                .inner_html();
+            closure.forget();
+        }
+    });
 
-            onchange.call(html.clone());
-        }) as Box<dyn FnMut(JsValue)>);
+    use_effect({
+        let id = id.clone();
+        let content = content.clone();
+        move || {
+            let js = format!(
+                r#"
+                (function syncContent() {{
+                    let editor = document.getElementById("{id}");
+                    if (editor && editor.__quill) {{
+                        let current = editor.__quill.root.innerHTML;
+                        let next = `{content}`;
+                        if (current !== next) {{
+                            editor.__quill.clipboard.dangerouslyPasteHTML(next);
+                        }}
+                    }}
+                }})();
+                "#
+            );
 
-        document
-            .add_event_listener_with_callback("content-updated", closure.as_ref().unchecked_ref())
-            .unwrap();
-
-        closure.forget();
+            spawn(async move {
+                let _ = eval(&js);
+            });
+        }
     });
 
     rsx! {
@@ -62,11 +101,11 @@ pub fn RichText(content: String, onchange: EventHandler<String>) -> Element {
             rel: "stylesheet",
             href: "https://cdn.quilljs.com/1.3.6/quill.snow.css",
         }
-
         document::Script { src: "https://cdn.quilljs.com/1.3.6/quill.min.js" }
+
         div {
-            class: "rich-text-editor w-full h-fit min-h-[100px] overflow-y-auto border-1 border-gray-300 rounded-md",
-            "data-default-value": "{content}",
+            id: "{id}",
+            class: "rich-text-editor w-full h-fit min-h-[100px] overflow-y-auto border border-gray-300 rounded-md",
         }
     }
 }
